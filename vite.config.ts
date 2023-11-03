439b2e5c82a94ffc6613bd129c6c517498ddb066
shenxuxiang <shenxuxiang@ahqianmo.com>
2023-11-03 13:36:54 +0800

    feat: 版本更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
 import postcssPresetEnv from 'postcss-preset-env';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
 import process from 'process';
 import path from 'path';
 
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve('src'),
       }
     },
-    plugins: [ react(), legacy(), splitVendorChunkPlugin() ],
+    plugins: [ react(), legacy() ],
     build: {
       outDir: 'build',
       cssMinify: true,
